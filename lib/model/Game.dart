730a1f718ebc8539d2d404c27f467b3561cbd47a
AwfulAlien <dhana22busy@gmail.com>
2025-02-28 23:42:22 +0530

    Added Game and Match modal classes

---
+++ lib/model/Game.dart
@@ -0,0 +1,7 @@
+class Game {
+  String player1Name;
+  String player2Name;
+  String? winner;
+
+  Game({required this.player1Name, required this.player2Name, this.winner});
+}
---
+++ lib/model/Match.dart
@@ -0,0 +1,8 @@
+import 'package:sock/model/Game.dart';
+
+class Match {
+  List<Game> games;
+  String? winner;
+
+  Match({required this.games});
+}
